8ea0324446f8034b9658ccc60d91f922f0944acd
pnakul6 <pnakul6@gmail.com>
2023-04-05 19:41:47 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,13 @@ export class Navbar extends Component {
                     <div className="App">
                         <nav className="navbar navbar-expand-lg bg-body-secondary">
                             <div className="container-fluid">
-                                <a className="navbar-brand" href="/">Start</a>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
                                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                         <li className="nav-item">
-                                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                            <a className="nav-link active" aria-current="page" href="/">Top Headlines</a>
                                         </li>
                                         <li className="nav-item dropdown">
                                             <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
